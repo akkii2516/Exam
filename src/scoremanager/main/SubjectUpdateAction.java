b9b356311821ed677350a384892b05a1ec66935a
a_shijo <a_shijo@10.202.18.165>
2025-04-18 13:43:59 +0900

    科目変更修正仮

---
+++ src/scoremanager/main/SubjectUpdateAction.java
@@ -0,0 +1,40 @@
+//仮段階
+package scoremanager.main;
+
+import java.util.List;
+
+import javax.security.auth.Subject;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import bean.Teacher;
+import tool.Action;
+
+public class SubjectUpdateAction extends Action {
+
+	@Override
+	public void execute(HttpServletRequest req, HttpServletResponse res) throws Exception {
+
+		HttpSession session = req.getSession();//セッション
+		Teacher teacher = (Teacher)session.getAttribute("user");
+
+		//subject_list.jspから変更を押した科目コードを取得
+		String cd = req.getParameter("CD");
+
+		//科目Daoを初期化
+		SubjectDao SubjectDao = new SubjectDao();
+		//科目コード、学校コードをもとに科目の詳細データを取得
+		Subject subject = SubjectDao.get(cd,teacher.getSchool());
+
+		//ログインユーザーの学校コードをもとに現在の科目名を取得
+		List<String> list = SubjectDao.filter(subject);
+
+		//リクエストにデータをセット
+		req.setAttribute("subject", subject);
+		req.setAttribute("list", list);
+
+		//指定されたJSPページ("subject_update.jsp")へフォワード
+		req.getRequestDispatcher("subject_update.jsp").forward(req, res);
+	}
+}
---
+++ src/scoremanager/main/SubjectUpdateExecuteAction.java
@@ -0,0 +1,31 @@
+//仮段階
+package scoremanager.main;
+
+import javax.security.auth.Subject;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import tool.Action;
+
+public class SubjectUpdateExecuteAction extends Action {
+    public void execute(HttpServletRequest req, HttpServletResponse res) throws Exception {
+
+    	String cd = "";
+    	String name = "";
+    	//科目Daoを初期化
+        Subject subject = new Subject();
+        SubjectDao subjectDao = new SubjectDao();
+
+      //リクエストにデータをセット
+        cd = req.getParameter("cd");
+        name = req.getParameter("name");
+
+        subject.setCd(cd);
+        subject.setName(name);
+
+        subjectDao.save(subject);
+
+        req.getRequestDispatcher("subject_update_done.jsp").forward(req, res);
+
+    }
+}
