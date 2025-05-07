976c3d24e01e82d5778245ab873920383bb7f129
a_shijo <a_shijo@10.202.18.165>
2025-05-07 11:25:41 +0900

    requestが大文字のCDになっていたためnullで返されたので小文字のcdに変更

--- src/scoremanager/main/SubjectUpdateAction.java
+++ src/scoremanager/main/SubjectUpdateAction.java
@@ -19,15 +19,19 @@ public class SubjectUpdateAction extends Action {
 		Teacher teacher = (Teacher)session.getAttribute("user");
 
 		//subject_list.jspから変更を押した科目コードを取得
-		String cd = req.getParameter("CD");
+		String cd = req.getParameter("cd");
 
 		//科目Daoを初期化
 		SubjectDao SubjectDao = new SubjectDao();
+		System.out.println("cd: " + cd);
+		System.out.println("school: " + teacher.getSchool());
 		//科目コード、学校コードをもとに科目の詳細データを取得
 		Subject subject = SubjectDao.get(cd,teacher.getSchool());
+		System.out.println(SubjectDao.get(cd,teacher.getSchool()));
 
 		//リクエストにデータをセット
 		req.setAttribute("subject", subject);
+		System.out.println(subject);
 
 		//指定されたJSPページ("subject_update.jsp")へフォワード
 		req.getRequestDispatcher("subject_update.jsp").forward(req, res);
