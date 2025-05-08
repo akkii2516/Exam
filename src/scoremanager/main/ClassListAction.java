790f2fc731452a5eda10676a321cd36f778aa0a2
djangouser <kaho.nama0918@gmail.com>
2025-05-08 11:38:36 +0900

    頭がおかしくなりそう

--- src/scoremanager/main/ClassListAction.java
+++ src/scoremanager/main/ClassListAction.java
@@ -1,5 +1,26 @@
 package scoremanager.main;
 
-public class ClassListAction {
 
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import bean.Teacher;
+import dao.ClassNumDao;
+import tool.Action;
+
+public class ClassListAction extends Action {
+    @Override
+    public void execute(HttpServletRequest req, HttpServletResponse res) throws Exception {
+        HttpSession session = req.getSession();
+        Teacher teacher = (Teacher) session.getAttribute("user");
+
+        ClassNumDao classNumDao = new ClassNumDao();
+        List<String> classNums = classNumDao.filter(teacher.getSchool());
+
+        req.setAttribute("classNums", classNums);
+        req.getRequestDispatcher("class_list.jsp").forward(req, res);
+    }
 }
