e3091dfc858cc1d6da7510564a13869f30c9957c
djangouser <kaho.nama0918@gmail.com>
2025-05-09 09:49:20 +0900

    ああああああああああ

--- src/scoremanager/main/ClassCreateAction.java
+++ src/scoremanager/main/ClassCreateAction.java
@@ -1,5 +1,28 @@
-package scoremanager.main;
 
-public class ClassCreateAction {
+	package scoremanager.main;
+	import java.util.List;
 
-}
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import bean.ClassNum;
+import bean.Teacher;
+import dao.ClassNumDao;
+import tool.Action;
+	public class ClassCreateAction extends Action {
+	    public void execute(HttpServletRequest req, HttpServletResponse res) throws Exception {
+	        // セッションからユーザー情報を取得
+	        HttpSession session = req.getSession();
+	        Teacher teacher = (Teacher) session.getAttribute("user");
+	        // 科目一覧を取得
+	        ClassNumDao classNumDao = new ClassNumDao();
+	        List<String> list = classNumDao.filter(teacher.getSchool());
+	        // 空のSubjectをリクエストにセット（フォーム用）
+	        ClassNum classnum = new ClassNum();
+	        req.setAttribute("ClassNum", classnum);
+	        req.setAttribute("class_list", list);
+	        // JSPへフォワード
+	        req.getRequestDispatcher("class_create.jsp").forward(req, res);
+	    }
+	}
